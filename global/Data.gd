3dd10e6124865d19c0cc33a867d2e52fb6ceb2b2
bunsbof <nguyenminhcuong2000@gmail.com>
2024-09-06 20:20:37 +0700

    #2024/09/06 - Added bar calculation

--- global/Data.gd
+++ global/Data.gd
@@ -1,11 +1,15 @@
 extends Node
 
 signal trust_level_changed
+signal point_changed
 
 var trust_level = 1:
 	set(value):
 		trust_level = value
 		emit_signal("trust_level_changed")
 
-var point: float = 70
+var point: float = 70:
+	set(value):
+		point = value
+		emit_signal("point_changed", point)
 var money: float = 2146
